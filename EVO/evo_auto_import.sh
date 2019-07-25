5852613e3abe3784fad9406cf2ca332fb5438238
SH-Goetch <53053667+SH-Goetch@users.noreply.github.com>
2019-07-25 10:42:51 -0400

    Update evo_auto_import.sh

--- EVO/evo_auto_import.sh
+++ EVO/evo_auto_import.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+# Calls POSIM EVO's Unattended Import Feature
+
+# Set the Path for the Import 
+
+IMPORT_PATH="/Library/FileMaker Server/Data/Documents/Evo/"
+
+cd "/Applications/POSIM EVO.app/Contents/Java/" && ./java -jar Juniper.jar --import $IMPORT_PATH
